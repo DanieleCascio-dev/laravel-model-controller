9cd365d0664216a444da4b8c8290772c69bb52f5
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-15 15:40:30 +0100

    add: navbar and style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Films</h1>
-    <ul class="d-flex row gy-5">
+
+    <ul class="d-flex row gy-5 p-3 w-100 align-items-center ">
      
         @foreach ($movies as $movie)
 
         <li class="col-4">
-            <div class="card" style="width: 18rem;">
+            <div class="card _bg-card ">
             <img src={{ $movie->image }} class="card-img-top" alt={{ $movie->title }}>
             <div class="card-body">
               <h5 class="card-title">{{ $movie->title }}</h5>
               <p class="card-text">
                 <ul class="list-group">
-                    <li class="list-group-item">Rating: {{ $movie->vote }}/10</li>
-                    <li class="list-group-item">Nation: {{ $movie->nationality }}</li>
+                    <li class="list-group-item "><span class="fw-bold">Rating:</span> {{ $movie->vote }}/10</li>
+                    <li class="list-group-item"><span class="fw-bold">Nation:</span> {{ $movie->nationality }}</li>
                     
                 </ul>
               </p>
