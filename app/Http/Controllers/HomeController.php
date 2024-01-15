fb731db496ca6981bdb9bd038cf1e32abb1ceadc
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-15 15:10:27 +0100

    add: show all movies in database into home.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     //
     public function index(){
         $movies = Movie::all();
-        dd($movies);
-        return view('home');
+        // dd($movies);
+        return view('home',compact('movies'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Welcome to laravel</h1>
+    <h1>Films</h1>
+    <ul class="d-flex row gy-5">
+     
+        @foreach ($movies as $movie)
+
+        <li class="col-4">
+            <div class="card" style="width: 18rem;">
+            <img src={{ $movie->image }} class="card-img-top" alt={{ $movie->title }}>
+            <div class="card-body">
+              <h5 class="card-title">{{ $movie->title }}</h5>
+              <p class="card-text">
+                <ul class="list-group">
+                    <li class="list-group-item">Rating: {{ $movie->vote }}/10</li>
+                    <li class="list-group-item">Nation: {{ $movie->nationality }}</li>
+                    
+                </ul>
+              </p>
+              <a href="https://www.google.com/search?q={{ $movie->title }}" target="_blank" class="btn btn-primary">Watch Now!</a>
+            </div>
+          </div>
+        </li>
+            
+        @endforeach
+    
+    </ul>
 @endsection
